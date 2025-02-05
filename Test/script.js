4b398ba079986097b82c17e755196d8cf1c06852
Shashank-Janagam <shashijanagam2004@gmail.com>
2025-02-05 17:41:21 +0530

    tested

--- Test/script.js
+++ Test/script.js
@@ -1,12 +1,13 @@
-// ✅ Initialize Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyCib5ywnEJvXXIePdWeKZtrKMIi2-Q_9sM",
+    authDomain: "geo-orbit-ed7a7.firebaseapp.com",
+    databaseURL: "https://geo-orbit-ed7a7-default-rtdb.firebaseio.com",
+    projectId: "geo-orbit-ed7a7",
+    storageBucket: "geo-orbit-ed7a7.firebasestorage.app",
+    messagingSenderId: "807202826514",
+    appId: "1:807202826514:web:5630f581f6f9dff46aebcb",
+    measurementId: "G-H15DN69132"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
